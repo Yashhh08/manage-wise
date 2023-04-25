e3fce0046fde14dc222cdd29900b3be90319a6b9
Yash Yerunkar <yashyerunkar8@gmail.com>
2023-04-25 17:31:45 +0530

    feat: add attendances component

---
+++ apps/manage-wise-client/src/app/components/attendances/attendances.component.ts
@@ -0,0 +1,120 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { Component, OnInit } from '@angular/core';
+import { AttendanceService } from './../../services/attendance.service';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { AttendanceFormComponent } from '../attendance-form/attendance-form.component';
+
+@Component({
+  selector: 'manage-wise-attendances',
+  templateUrl: './attendances.component.html',
+  styleUrls: ['./attendances.component.scss'],
+})
+export class AttendancesComponent implements OnInit {
+
+  attendances!: any;
+
+  filteredAttendance!: any;
+
+  searchTerm = '';
+
+  ref!: DynamicDialogRef;
+
+  constructor(private attendanceService: AttendanceService, private confirmationService: ConfirmationService, private messageService: MessageService, private dialogService: DialogService) { }
+
+  ngOnInit(): void {
+    this.getAllAttendaces();
+  }
+
+  filterByName() {
+    this.filteredAttendance = this.attendances.filter((attendance: any) => {
+      return attendance.employeeId.name.toLowerCase().includes(this.searchTerm.toLowerCase());
+    });
+  }
+
+  addAttendance() {
+    this.ref = this.dialogService.open(AttendanceFormComponent, {
+      // header: 'Select a Product',
+      width: '70%',
+      contentStyle: { overflow: 'auto' },
+      baseZIndex: 10000,
+      maximizable: true
+    });
+
+    this.ref.onClose.subscribe((product: any) => {
+      if (product) {
+        this.messageService.add({ severity: 'info', summary: 'Product Selected', detail: product.name });
+      }
+      this.getAllAttendaces();
+    });
+
+    this.ref.onMaximize.subscribe((value: any) => {
+      this.messageService.add({ severity: 'info', summary: 'Maximized', detail: `maximized: ${value.maximized}` });
+    });
+  }
+
+  getAllAttendaces() {
+    this.attendanceService.getAttendances(0, 0).subscribe((res) => {
+      // console.log(res);
+      
+      this.attendances = res;
+      this.filteredAttendance = this.attendances;
+    }, (err) => {
+      console.log(err);
+    })
+  }
+
+  confirmUpdate(event: any, attendance: any) {
+    this.confirmationService.confirm({
+      target: event.target,
+      message: 'Are you sure that you want to proceed?',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.updateAttendance(attendance);
+      },
+      reject: () => {
+      }
+    });
+  }
+
+  confirmDelete(event: any, attendance: any) {
+    this.confirmationService.confirm({
+      target: event.target,
+      message: 'Are you sure that you want to proceed?',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.attendanceService.deleteAttendance(attendance._id).subscribe((res) => {
+          this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Record deleted Successfully' });
+          this.getAllAttendaces();
+        }, (err) => {
+          this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Something went wrong' });
+        })
+      },
+      reject: () => {
+      }
+    });
+  }
+
+  updateAttendance(attendance: any) {
+    this.ref = this.dialogService.open(AttendanceFormComponent, {
+      // header: 'Select a Product',
+      width: '70%',
+      contentStyle: { overflow: 'auto' },
+      baseZIndex: 10000,
+      maximizable: true,
+      data: attendance
+    });
+
+    this.ref.onClose.subscribe((product: any) => {
+      if (product) {
+        this.messageService.add({ severity: 'info', summary: 'Product Selected', detail: product.name });
+      }
+      this.getAllAttendaces();
+    });
+
+    this.ref.onMaximize.subscribe((value) => {
+      this.messageService.add({ severity: 'info', summary: 'Maximized', detail: `maximized: ${value.maximized}` });
+    });
+  }
+
+}
